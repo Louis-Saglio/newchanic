a7d331d10f0c25fc0586c0de00a260b75bc41e62
Louis-Saglio <louis.saglio@sfr.fr>
2019-10-19 10:58:18 +0200

    Total rewrite to support virtual simultaneous particle state update Dropped support for law and text display

--- newchanic/graphical_engine.py
+++ newchanic/graphical_engine.py
@@ -1,216 +1,93 @@
-# todo : split engine from graphical engine
-# todo : remove is_alive from Particle
-# todo : remove dead particle
-# todo : display only if in screen
-
-import time
+from random import randint
 from tkinter import Tk
-from typing import List, Tuple
+from typing import Tuple, Optional, List
 
 import pygame
 
-from .physics import Particle, Law
-from .utils import Number
+from engine import Engine
+from physics import Particle
+from utils import Number
 
 
-class GraphicalParticle(Particle):
-    def __init__(self, universe: "Universe", mass: Number, position: List[Number], velocity: List[Number]):
+class CachedPropertiesMixin:
+    def get_cached(self, attr: str):
+        return getattr(self, f"_{attr}", None)
+
+    def get_updated(self, attr: str):
+        value = getattr(self, f"compute_{attr}")()
+        setattr(self, f"_{attr}", value)
+        return value
+
+
+class GraphicalOptions:
+    def __init__(self, window_size: Tuple[Number, Number] = None):
+        self.size = self.get_window_size(window_size)
+        self.zoom_level: Number = 1
+        self.represented_dimensions: Tuple[Number, Number] = (0, 1)
+        self.background_color = (0, 0, 0)
+
+    @staticmethod
+    def get_window_size(window_size: Optional[Tuple[Number, Number]] = None):
+        if window_size:
+            return window_size
+        screen = Tk()
+        return screen.winfo_screenheight(), screen.winfo_screenwidth()
+
+
+class GraphicalParticle(Particle, CachedPropertiesMixin):
+    def __init__(self, mass: Number, position: List[Number], velocity: List[Number], options: GraphicalOptions):
         super().__init__(mass, position, velocity)
-        self.universe = universe
-        self.old_graphical_position = [0, 0]
-        self.present_graphical_position = [0, 0]
+        self.options = options
 
     @property
-    def color(self) -> Tuple[Number, Number, Number]:
-        raise NotImplementedError
+    def color(self):
+        return randint(0, 255), randint(0, 255), randint(0, 255)
 
-    @property
-    def dimension(self) -> Number:
-        raise NotImplementedError
+    def compute_size(self) -> int:
+        return 3
 
-    @property
-    def graphical_dimension(self):
-        return max(1, self.dimension / self.universe.zoom_level)
-
-    @property
-    def graphical_position(self):
-        self.old_graphical_position = self.present_graphical_position
-        self.present_graphical_position = (
-            (self._position[0] / self.universe.zoom_level) + (self.universe.width / 2),
-            (self._position[1] / self.universe.zoom_level) + (self.universe.height / 2),
+    def compute_graphical_position(self) -> Tuple[Number, Number]:
+        return (
+            (self._position[self.options.represented_dimensions[0]] / self.options.zoom_level)
+            + (self.options.size[0] / 2),
+            (self._position[self.options.represented_dimensions[1]] / self.options.zoom_level)
+            + (self.options.size[1] / 2),
         )
-        return self.present_graphical_position
 
 
-class Universe:
-    def __init__(
-        self,
-        width: Number = None,
-        height: Number = None,
-        zoom_level: Number = 1,
-        draw_trajectory=False,
-        sync_time=False,
-    ):
-        pygame.init()
-        self.zoom_level = zoom_level  # 1 is normal, 2 is twice un-zoomed, 0.5 is twice zoomed
-        self.sync_time = sync_time
-        self.draw_trajectory = draw_trajectory
-        if width is None or height is None:
-            screen = Tk()
-        self.height = height or screen.winfo_screenheight()
-        self.width = width or screen.winfo_screenwidth()
-        self._units: List[GraphicalParticle] = []
-        self._window = pygame.display.set_mode((self.width, self.height))
-        self.laws: List[Law] = []
-        self._next_zoom_delta = 0
-        self._shift = [0, 0]
-        self._next_shift = [0, 0]
-        self._texts_to_display = []
-        self._texts_to_erase = []
-        self._font_size = 20
-        self._font = pygame.font.SysFont(None, self._font_size)
-        self.must_erase_screen = False
+class GraphicalEngine(Engine):
+    def __init__(self, window_size: Tuple[Number, Number] = None):
+        self.particles: List[GraphicalParticle]
+        self.options = GraphicalOptions(window_size)
+        self._window = pygame.display.set_mode(self.options.size)
+        super().__init__(GraphicalParticle, {"options": self.options})
 
-    def add_unit(self, particle: GraphicalParticle):
-        self._units.append(particle)
+    def run_custom_engine_features(self):
+        self.update_particles()
 
-    def erase_units(self) -> None:
-        for particle in self._units:
-            if particle.is_alive or True:
-                gd = round(particle.graphical_dimension)
-                pygame.draw.circle(
-                    self._window,
-                    (0, 0, 0),
-                    (
-                        round(particle.old_graphical_position[0] + self._shift[0]),
-                        round(particle.old_graphical_position[1] + self._shift[1]),
-                    ),
-                    gd * 2,
-                    gd * 2,
-                )
+    def update_particles(self):
+        particle: GraphicalParticle
+        for particle in self.particles:
+            pygame.draw.circle(
+                self._window,
+                particle.color,
+                particle.get_updated("graphical_position"),
+                particle.get_updated("size"),
+                particle.get_cached("size"),
+            )
 
-    def render_units(self) -> None:
-        for particle in self._units:
-            if particle.is_alive:
-                gp = particle.graphical_position
-                pygame.draw.circle(
-                    self._window,
-                    particle.color,
-                    (round(gp[0] + self._shift[0]), round(gp[1] + self._shift[1])),
-                    round(particle.graphical_dimension),
-                    round(particle.graphical_dimension),
-                )
+    def erase_particles(self):
+        particle: GraphicalParticle
+        for particle in self.particles:
+            pygame.draw.circle(
+                self._window,
+                self.options.background_color,
+                particle.get_cached("graphical_position"),
+                particle.get_cached("size"),
+                particle.get_cached("size"),
+            )
 
-    def apply_laws(self):
-        for law in self.laws:
-            for particle in self._units:
-                if particle.is_alive:
-                    for other_particle in self._units:
-                        if other_particle.is_alive and particle != other_particle:
-                            particle.apply_force(law.compute_force(particle, other_particle))
 
-    def update_particle_positions(self):
-        for unit in self._units:
-            if unit.is_alive:
-                unit.exist()
-
-    def ask_for_zoom(self, level_delta):
-        if self._next_zoom_delta + level_delta + self.zoom_level >= 1:
-            self._next_zoom_delta += level_delta
-
-    def zoom(self):
-        if self._next_zoom_delta and self.draw_trajectory:
-            self.ask_for_erase_screen()
-        self.zoom_level += self._next_zoom_delta
-        self._next_zoom_delta = 0
-
-    def ask_for_erase_screen(self):
-        self.must_erase_screen = True
-
-    def erase_screen(self):
-        if self.must_erase_screen:
-            pygame.draw.rect(self._window, (0, 0, 0), ((0, 0), (self.width, self.height)))
-            self.must_erase_screen = False
-
-    def ask_for_shift(self, shift_level):
-        self._next_shift = shift_level
-
-    def shift(self):
-        if self._next_shift != [0, 0]:
-            self.ask_for_erase_screen()
-        self._shift[0] += self._next_shift[0]
-        self._shift[1] += self._next_shift[1]
-        self._next_shift = [0, 0]
-
-    def display_text(self):
-        next_pos = [0, 0]
-        while self._texts_to_display:
-            text = self._texts_to_display.pop(0)
-            self._texts_to_erase.append(text)
-            text_surface = self._font.render(text, True, (255, 255, 255), (0, 0, 0))
-            self._window.blit(text_surface, next_pos)
-            next_pos[1] += self._font_size
-
-    def erase_text(self):
-        next_pos = [0, 0]
-        while self._texts_to_erase:
-            text = self._texts_to_erase.pop(0)
-            text_surface = self._font.render(text, True, (255, 255, 255), (255, 255, 255))
-            self._window.blit(text_surface, next_pos)
-            next_pos[1] += self._font_size
-
-    def ask_for_text_display(self, text: str):
-        self._texts_to_display.append(text)
-
-    def loop(self):
-        run = True
-        total_time = 0
-        while run:
-            try:
-                total_time += 1
-
-                if not self.draw_trajectory:
-                    self.erase_units()
-                self.erase_screen()
-                self.zoom()
-                self.shift()
-                self.render_units()
-                self.apply_laws()
-                self.update_particle_positions()
-
-                self.ask_for_text_display(str(total_time))
-                alive_particles = [p for p in self._units if p.is_alive]
-                self.ask_for_text_display(str(len(alive_particles)))
-                self.ask_for_text_display(
-                    str(round(sum([(sum(p._velocity) / 2) * p._mass for p in alive_particles]), 5))
-                )
-
-                self.erase_text()
-                self.display_text()
-
-                pygame.display.flip()
-
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        run = False
-                    elif event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 4:  # scroll up
-                            self.ask_for_zoom(-1)
-                        elif event.button == 5:  # scroll down
-                            self.ask_for_zoom(1)
-                    elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_LEFT:
-                            self.ask_for_shift((-100, 0))
-                        elif event.key == pygame.K_RIGHT:
-                            self.ask_for_shift((100, 0))
-                        elif event.key == pygame.K_UP:
-                            self.ask_for_shift((0, -100))
-                        elif event.key == pygame.K_DOWN:
-                            self.ask_for_shift((0, 100))
-
-                if self.sync_time:
-                    time.sleep(0.01)
-
-            except KeyboardInterrupt:
-                run = False
-        print(total_time)
+if __name__ == "__main__":
+    engine = GraphicalEngine()
+    engine.run()
