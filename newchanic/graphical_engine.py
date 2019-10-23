31a4749f2e1cede7503a65677d193e060d605ba9
Louis-Saglio <louis.saglio@sfr.fr>
2019-10-23 23:11:03 +0200

    Clear screen before zooming or moving in screen

--- newchanic/graphical_engine.py
+++ newchanic/graphical_engine.py
@@ -29,10 +29,11 @@ class CachedPropertiesMixin:
 class GraphicalOptions:
     def __init__(self, window_size: Tuple[Number, Number] = None):
         self.size = self.get_window_size(window_size)
-        self.zoom_level: Number = 1
+        self.zoom_level: Number = 5
         self.represented_dimensions: Tuple[Number, Number] = (0, 1)
         self.background_color = (0, 0, 0)
         self.shift_level = [0, 0]
+        self.draw_trajectories = True
 
     @staticmethod
     def get_window_size(window_size: Optional[Tuple[Number, Number]] = None) -> Tuple[Number, Number]:
@@ -48,7 +49,8 @@ class GraphicalParticle(Particle, CachedPropertiesMixin):
         self.options = options
         self.old_position: List[Number]
 
-    def compute_color(self):
+    @staticmethod
+    def compute_color():
         return randint(0, 255), randint(0, 255), randint(0, 255)
 
     def compute_size(self) -> int:
@@ -57,14 +59,15 @@ class GraphicalParticle(Particle, CachedPropertiesMixin):
     def compute_graphical_position(self) -> Tuple[Number, Number]:
         return (
             (self._position[self.options.represented_dimensions[0]] / self.options.zoom_level)
-            + (self.options.size[0] / 2) + self.options.shift_level[0],
+            + (self.options.size[0] / 2)
+            + self.options.shift_level[0],
             (self._position[self.options.represented_dimensions[1]] / self.options.zoom_level)
-            + (self.options.size[1] / 2) + self.options.shift_level[1],
+            + (self.options.size[1] / 2)
+            + self.options.shift_level[1],
         )
 
 
-class GraphicalEngine(Engine):
-
+class GraphicalEngine2D(Engine):
     def __init__(self, window_size: Tuple[Number, Number] = None):
         self._event_listeners: Dict[int, Callable] = {
             pygame.QUIT: self.quit,
@@ -74,17 +77,18 @@ class GraphicalEngine(Engine):
         self.particles: List[GraphicalParticle]
         self.options = GraphicalOptions(window_size)
         self._window = pygame.display.set_mode(self.options.size)
+        self._must_erase = False
         super().__init__(GraphicalParticle, {"options": self.options})
 
     def run_custom_engine_features(self):
-        try:
+        if self._must_erase:
             self.erase_particles()
-        except TypeError:
-            pass
+        self._must_erase = not self.options.draw_trajectories
         self.update_particles()
         for event in pygame.event.get():
             event_listener = self._event_listeners.get(event.type)
             if callable(event_listener):
+                print(event)
                 event_listener(event)
         pygame.display.flip()
 
@@ -96,16 +100,18 @@ class GraphicalEngine(Engine):
             self.options.zoom_level *= 0.9
         elif event.button == 5:
             self.options.zoom_level *= 1.1
+        self._must_erase = True
 
     def shift_view(self, event):
         if event.key == pygame.K_DOWN:
-            self.options.shift_level[1] += 100
-        elif event.key == pygame.K_UP:
             self.options.shift_level[1] -= 100
+        elif event.key == pygame.K_UP:
+            self.options.shift_level[1] += 100
         elif event.key == pygame.K_RIGHT:
-            self.options.shift_level[0] += 100
-        elif event.key == pygame.K_LEFT:
             self.options.shift_level[0] -= 100
+        elif event.key == pygame.K_LEFT:
+            self.options.shift_level[0] += 100
+        self._must_erase = True
 
     def update_particles(self):
         particle: GraphicalParticle
@@ -120,16 +126,9 @@ class GraphicalEngine(Engine):
 
     def erase_particles(self):
         particle: GraphicalParticle
-        for particle in self.particles:
-            pygame.draw.circle(
-                self._window,
-                self.options.background_color,
-                [int(i) for i in particle.get_cached("graphical_position")],
-                particle.get_cached("size"),
-                particle.get_cached("size"),
-            )
+        self._window.fill(self.options.background_color)
 
 
 if __name__ == "__main__":
-    engine = GraphicalEngine()
+    engine = GraphicalEngine2D()
     engine.run()
